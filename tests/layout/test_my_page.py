bc9ab9b67dbc6660d433ef9bd8d89050d9fdd0fc
FrancoisCapon <github@francoiscapon.com>
2025-12-15 13:40:13 +0100

    exception

--- tests/layout/test_my_page.py
+++ tests/layout/test_my_page.py
@@ -2122,6 +2122,21 @@ def test_page_size_fit_page_multidiv_2():
     assert int(page.width) == 11 + 5 + 555 + 5 + 33 + 1
     assert int(page.height) == 22 + 7 + 333 + 666 + 7 + 44 + 1
 
+@assert_no_logs
+def test_page_size_fit_one_cant_disabled_more_than_one_page():
+    with pytest.raises(ValueError) as value_error:
+      pages = render_pages(
+        """
+        <style>
+        @page { size: fit; }
+        div {width: 60px; height: 50px}
+        </style>
+        <div></div>
+        <div style="break-before: page !important"></div>
+        """
+      )
+    assert 
+
 # 96px / in 1 px = 1/96 in = 0,026458 cm
 # 1 in (pouce) = 96 px CSS = 2.54 cm
 
