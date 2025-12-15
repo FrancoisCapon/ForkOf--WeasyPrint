94c5bb583455f0206b436541a53b7fadeee94a94
FrancoisCapon <github@francoiscapon.com>
2025-12-15 02:36:49 +0100

    page_multidiv_1():     pages = render_pages(

--- tests/layout/test_my_page.py
+++ tests/layout/test_my_page.py
@@ -2077,13 +2077,13 @@ def test_page_size_fit_page_content_box():
     assert int(page.height) == 22 + 10 + 3 + 7 + 200 + 7 + 3 + 10 + 44 + 1
 
 @assert_no_logs
-def test_page_size_fit_page_complexe_1():
+def test_page_size_fit_page_multidiv_1():
     pages = render_pages(
       """
       <style>
       @page { size: fit; margin-left: 11px; margin-top: 22px; margin-right: 33px; margin-bottom: 44px}
       * {margin: 0; padding: 0}
-      div {width: 100px; height: 200px; margin: 10px; padding: 7px; border: 3px;box-sizing: border-box;}
+      div {width: 100px; height: 200px; margin: 10px; padding: 7px; border: 3px solid;box-sizing: border-box;}
       </style>
       <body>
       <div></div>
