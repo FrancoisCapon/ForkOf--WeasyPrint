ccb2cbc624aeb4d662891399dac3cc3913a13400
FrancoisCapon <github@francoiscapon.com>
2025-12-15 01:45:11 +0100

    yea border ?

--- tests/layout/test_my_page.py
+++ tests/layout/test_my_page.py
@@ -1980,23 +1980,6 @@ def test_page_size_fit_one_element():
     assert int(page.height) == 333 + 1
 
 @assert_no_logs
-def test_page_size_fit_one_element_with_page_margins():
-    pages = render_pages(
-      """
-      <style>
-      @page { size: fit; margin-left: 11px; margin-top: 22px; margin-right: 33px; margin-bottom: 44px}
-      * {margin: 0; padding: 0}
-      div {width: 100px; height: 200px}
-      </style>
-      <div></div>
-      """
-    )
-    assert len(pages) == 1
-    page = pages[0]
-    assert int(page.width) == 11 + 100 + 33 + 1
-    assert int(page.height) == 22 + 200 + 44 + 1
-
-@assert_no_logs
 def test_page_size_fit_no_page_break_before(): 
     pages = render_pages(
       """
@@ -2038,7 +2021,41 @@ def test_page_size_fit_no_page_break_after():
     assert int(page.width) == 333 + 1
     assert int(page.height) == 4 * 444 + 1
 
+@assert_no_logs
+def test_page_size_fit_one_element_with_page_margins():
+    pages = render_pages(
+      """
+      <style>
+      @page { size: fit; margin-left: 11px; margin-top: 22px; margin-right: 33px; margin-bottom: 44px}
+      * {margin: 0; padding: 0}
+      div {width: 100px; height: 200px}
+      </style>
+      <div></div>
+      """
+    )
+    assert len(pages) == 1
+    page = pages[0]
+    assert int(page.width) == 11 + 100 + 33 + 1
+    assert int(page.height) == 22 + 200 + 44 + 1
 
+@assert_no_logs
+def test_page_size_fit_complexe_page_1():
+    pages = render_pages(
+      """
+      <style>
+      @page { size: fit; margin-left: 11px; margin-top: 22px; margin-right: 33px; margin-bottom: 44px}
+      * {margin: 0; padding: 0}
+      div {width: 100px; height: 200px; margin: 0px; padding: 7px; box-sizing: border-box;}
+      </style>
+      <body>
+      <div></div>
+      </body>
+      """
+    )
+    assert len(pages) == 1
+    page = pages[0]
+    assert int(page.width) == 11 + 100 + 33 + 1
+    assert int(page.height) == 22 + 200 + 44 + 1
 
 # 96px / in 1 px = 1/96 in = 0,026458 cm
 # 1 in (pouce) = 96 px CSS = 2.54 cm
--- weasyprint/document.py
+++ weasyprint/document.py
@@ -285,8 +285,9 @@ class Document:
         for box in boxes:
             box_tag = getattr(box, "element_tag", None)
             if box_tag not in ['body', 'style', 'meta', 'script']:
-                width = max(width, box.position_x + box.width)
-                height = max(height, box.position_y + box.height)
+                width = max(width, box.position_x + box.padding_left + box.width + box.padding_right)
+                height = max(height, box.position_y + box.padding_top + box.height + box.padding_bottom)
+            print(box, box.position_x, box.padding_left)
         # +1 : avoid content "null" (1px css = 0,26 mm)
         # +1 : “avoid calculation errors 
         return width + page_box.margin_right + 1, height + page_box.margin_bottom + 1
